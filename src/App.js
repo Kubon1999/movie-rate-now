c1ec6c8b810545185c57e92d4cc6bbecb90c5db4
Kubon1999 <kuba.wolodzko@gmail.com>
2022-05-05 16:34:55 +0200

    added firebase configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import  { useAuthState }  from 'react-firebase-hooks/auth';
+
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDVrBdO44BUwk5YcqWFkdpVGsJUwkawGas",
+  authDomain: "movie-rate-now.firebaseapp.com",
+  projectId: "movie-rate-now",
+  storageBucket: "movie-rate-now.appspot.com",
+  messagingSenderId: "836171860164",
+  appId: "1:836171860164:web:19b7e7064506351eea2191",
+  measurementId: "G-LNKNC93RZ4"
+});
 
 function App() {
   return (
